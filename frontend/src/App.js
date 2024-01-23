50e5ce6c37d7ba426691573b2e3ba54684ea4bc7
singhkushagra01 <119737753+singhkushagra01@users.noreply.github.com>
2024-01-23 18:43:27 +0530

    Add files via upload
    npm i axios
    npm install
    npm start

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chatbot from './Chatbot';
+
+function App() {
+  return (
+    <div>
+      <h1>AI Chatbot</h1>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
